2068e2b95b64c93c6b642abd015d3e5a0dafcb44
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2022-06-19 22:48:29 -0600

    init to script

--- Js/Script.js
+++ Js/Script.js
@@ -903,6 +903,19 @@ class MatrizDispersa {
     }
 }
 
+//inicialización
+if(localStorage.getItem('usersCharged') == null) {
+    localStorage.setItem('usersCharged',JSON.stringify([JSON.parse(JSON.stringify(new Usuario(2354168452525,'Wilfred Perez','Wilfred','wilfred@bitrex.com','Administrador','123','+502 (123) 123-4567',0)))]))
+}
+
+if(localStorage.getItem('authorsCharged') == null) {
+    localStorage.setItem('authorsCharged','')
+}
+
+if(localStorage.getItem('booksCharged') == null) {
+    localStorage.setItem('booksCharged','')
+}
+
 //borrar registros
 function reset() {
     if(prompt('¿Reiniciar sistema? (y/n)') == 'y') {
